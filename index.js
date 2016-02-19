066658c5c0110c78e2e86be5789330bc2cf6bb80
michael-lopiccolo <m.lopiccolo3@gmail.com>
2016-02-18 23:27:58 -0600

    Tested EJS dynamic rendering

--- index.js
+++ index.js
@@ -15,24 +15,31 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/cool', function(request, response) {
-	response.send(cool());
+app.get('/profile\/*', function(request, response) {
+  var cut = request.originalUrl.substring(9);
+  response.render('pages/profile', {user: cut});
 });
 
-app.get('/db', function(request,response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result){
-			done();
-			if(err)
-				{console.error(err); response.send("Error "+err);}
-			else
-				{response.render('pages/db', {results: result.rows});}
-		});
-	});
+// app.get('/db', function(request,response) {
+// 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+// 		client.query('SELECT * FROM test_table', function(err, result){
+// 			done();
+// 			if(err)
+// 				{console.error(err); response.send("Error "+err);}
+// 			else
+// 				{response.render('pages/db', {results: result.rows});}
+// 		});
+// 	});
+// });
+
+app.get(/user\/*/, function(request, response) {
+  if(request.originalUrl === "/user/michael")
+  	response.send("u a cool guy");
+  else
+  	response.send("might be cool i dunno tbh");
+  // response.send('/a/');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
