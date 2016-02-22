6df9a620d974c13b4c8170b6425aca7811585ba6
Krystal <kculp13@gmail.com>
2016-02-21 19:43:37 -0600

    User object and functions created

--- backend.js
+++ backend.js
@@ -1,1 +1,26 @@
+'use strict';
 exports.coolfunction = function() {return "what"};
+exports.addUser = function(name) {console.log(name + "created!")};
+exports.getUser = function(name) {console.log(name + "returned!")};
+exports.removeUser = function(name) {console.log(name + "removed!")};
+
+class User{
+	constructor(name){
+		this.name = name;
+		this.friends = new Array();
+		this.marks = new Array();
+		this.tags = new Array();
+	}
+	
+	addFriend(friend){
+		this.friends.push(this.friend);
+	}
+	
+	addMark(mark){
+		this.marks.push(this.mark);
+	}
+	
+	addTag(tag) {
+		this.tags.push(this.tag);
+	}
+}
