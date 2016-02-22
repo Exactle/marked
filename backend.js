7d8be44ff35a06fc59c74f5e37bde0e8e7a8a3ad
michael-lopiccolo <m.lopiccolo3@gmail.com>
2016-02-21 20:13:13 -0600

    gave addUser a return value
    it returns the added user

--- backend.js
+++ backend.js
@@ -1,22 +1,35 @@
 'use strict';
 
-users = new Map();
+var users = new Map();
 
-// exports.coolfunction = function() {return "what"};
+//any function on the "exports" object can be called in index.js like so:
+//var backend = require('backend');
+//backend.coolfunction();
+
+exports.coolfunction = function() {
+	return "what"
+};
+
 exports.addUser = function(name) {
-	users.set(name, new User(name));
+	var added = new User(name); //we should check for if the user already exists
+	users.set(name, added);
 
 	console.log(name + "created!");
+	return added;
 };
+
 exports.getUser = function(name) {
 	console.log(name + "returned!");
 
 	return users.get(name);
 };
+
 exports.removeUser = function(name) {
 	users.delete(name);
 	
 	console.log(name + "removed!");
+
+	//maybe return some value here?
 };
 
 
