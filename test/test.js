c276c1d6c20bcf50f09e25b13359400b26d330e8
michael-lopiccolo <m.lopiccolo3@gmail.com>
2016-04-16 22:43:23 -0500

    Start testing testing

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+
+describe('signup and login', function() {
+	it('should allow user to make a new account')
+	it('should not allow account to be created that already exists')
+	it('should authenticate user with the correct password')
+	it('should not authenticate user with the wrong password')
+}
